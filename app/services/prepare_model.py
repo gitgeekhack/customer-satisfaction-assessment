ec68150a6eb43d9325be4675c4586a3b48a27078
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2022-03-17 09:58:05 +0530

    files updated.

--- app/services/prepare_model.py
+++ app/services/prepare_model.py
@@ -1,6 +1,5 @@
 from transformers import pipeline
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
 from app.constants import model_name
 
 sentiment_pipeline = pipeline('sentiment-analysis', model=model_name)
