cb6890dd506ee7b6ce99ddd921e9237f5249c88e
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2022-06-10 15:56:16 +0530

    model integrated in web app.

--- app/constant.py
+++ app/constant.py
@@ -1,3 +1,3 @@
-MODEL_PATH = "app/model/bertweet-base-sentiment-analysis"
+MODEL_PATH = "app/model/sentiment_analysis"
 REMOVE_UNWANTED_SYMBOLS = "[^a-zA-Z0-9(+*) \n\.]"
 REMOVE_UNWANTED_SPACES = "\s+"
